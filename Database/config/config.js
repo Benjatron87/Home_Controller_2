fcb27c3815c1012ebc18691b054f28b991d358f3
Benjatron <benjt122@gmail.com>
2020-09-13 13:32:40 -0400

    whoop

--- Database/config/config.js
+++ Database/config/config.js
@@ -18,7 +18,7 @@ module.exports = {
     "dialect": "mysql",
     "username": process.env.MYSQL_USER,
     "password": process.env.MYSQL_KEY,
-    "database": process.env.MYSQL_DBNAME,
+    "database": process.env.JAWSDB_URL,
     "host": process.env.MYSQL_HOST
   }
 }
